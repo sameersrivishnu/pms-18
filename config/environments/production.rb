a4d327cc068e9f650b30a4cc0fa204b437b84941
Sameer Sri Vishnu <sameersrivishnu@gmail.com>
2021-04-06 23:00:50 +0530

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "pms_18_production"
+  # config.active_job.queue_name_prefix = "devise_admin_app_production"
 
   config.action_mailer.perform_caching = false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :posts
+  root to: "posts#index"
+
+devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+}
+       delete '/shared_user_delete' => "posts#shared_user"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
